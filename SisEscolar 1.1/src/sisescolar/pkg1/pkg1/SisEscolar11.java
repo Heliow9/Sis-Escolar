310ece5e533d6d023706d80625c76d2f337bde70
Helio Cezar <heliow9@hotmail.com>
2017-12-19 21:02:57 -0200

    Alteração Helio 1.2

---
+++ SisEscolar 1.1/src/sisescolar/pkg1/pkg1/SisEscolar11.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sisescolar.pkg1.pkg1;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Helio Cezar
+ */
+public class SisEscolar11 extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.resizableProperty().setValue(Boolean.FALSE);
+        Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+        ;
+        
+ 
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
